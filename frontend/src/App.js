6418058960a2ba87a787528dad4b3d525c28c85b
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-03-13 23:18:39 +0500

    Added Header & Footer.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to E-Shop</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to E-Shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
