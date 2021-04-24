933a27f3d2fd23105d57f8d6a8aabef85205e4f2
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-04-24 22:54:15 +0500

    Cart Screen Done.

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -16,7 +16,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
         }
     })
     // Saved cart item to localstorage too.
-    localStorage.setItem('cartItems', JSON.stringify(getState.cart.cartItems ));
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems ));
 
 
 }
