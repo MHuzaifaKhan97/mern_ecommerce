5cd714c8969726baf7fc2805ecc74c6b53d9ac3b
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-04-24 21:19:15 +0500

    Cart Action, Reducer, Constant & changes in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,24 @@
-import { createStore, combineReducers , applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import {productDetailReducer, productListReducers} from './reducers/productReducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productDetailReducer, productListReducers } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducer';
 
 const reducer = combineReducers({
-    productList:productListReducers,
-    productDetails:productDetailReducer,
+    productList: productListReducers,
+    productDetails: productDetailReducer,
+    cart: cartReducer,
 });
 
-const initialState = {};
+// get local cart item stored in local storage.
+const cartItemFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
+
+const initialState = {
+    cart: { cartItems: cartItemFromStorage }
+};
 
 const middleware = [thunk];
-const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
