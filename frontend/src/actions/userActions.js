7b5991397dd50eb0875119de6783c89e48a2fe84
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-05-01 20:47:48 +0500

    Show user in Navbar & Logout.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAILED, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import { USER_LOGIN_FAILED, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
 import axios from 'axios';
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -27,3 +27,20 @@ export const login = (email, password) => async (dispatch) => {
         })
     }
 }
+
+export const logout = () => (dispatch)=>{
+    localStorage.removeItem('userInfo');
+    dispatch({
+        type:USER_LOGOUT,
+    })
+
+}
+
+
+
+
+
+
+
+
+
