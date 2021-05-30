dd7acb0c6a18575cb82eb661a15e538eb374d863
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-05-30 20:07:16 +0500

    Frontend Product Reviews.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILED, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAILED, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAILED, PRODUCT_DELETE_REQUEST, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAILED, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAILED } from '../constants/productConstants';
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILED, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAILED, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAILED, PRODUCT_DELETE_REQUEST, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAILED, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAILED, PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAILED } from '../constants/productConstants';
 
 // thunk middleware work here
 export const listProducts = () => async (dispatch) => {
@@ -105,11 +105,11 @@ export const updateProduct = (product) => async (dispatch, getState) => {
 
         const config = {
             headers: {
-                'Content-Type':'application/json',
+                'Content-Type': 'application/json',
                 Authorization: `Bearer ${userInfo.token}`
             }
         }
-        const { data } = await axios.put(`/api/products/${product._id}`,product, config);
+        const { data } = await axios.put(`/api/products/${product._id}`, product, config);
         dispatch({
             type: PRODUCT_UPDATE_SUCCESS,
             payload: data
@@ -118,6 +118,35 @@ export const updateProduct = (product) => async (dispatch, getState) => {
     catch (error) {
         dispatch({
             type: PRODUCT_UPDATE_FAILED,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message :
+                error.message
+        })
+    }
+}
+
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_REQUEST,
+        })
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        await axios.post(`/api/products/${productId}/reviews`, review, config);
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_SUCCESS,
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_FAILED,
             payload: error.response && error.response.data.message ?
                 error.response.data.message :
                 error.message
